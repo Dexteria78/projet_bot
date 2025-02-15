a027009911d0b15661849e109cf98df3d45d1bb6
nicolas <nicolas@DebianBot.lan>
2025-02-15 23:05:36 +0100

    Correction de la logique : notifications uniquement si en stock

--- scraper.py
+++ scraper.py
@@ -9,14 +9,14 @@ def check_availability(article):
 
     if "cultura.com" in url:
         status = soup.find("p", class_="stock color-red")
-        return status is None or "INDISPONIBLE" not in status.get_text(strip=True)
+        return status is not None and "INDISPONIBLE" in status.get_text(strip=True)
 
     if "fnac.com" in url:
         status = soup.find("div", class_="f-buyBox-availabilityStatus")
-        return status is not None and "disponible" in status.get_text(strip=True).lower()
+        return status is None or "indisponible" not in status.get_text(strip=True).lower()
 
     if "lerepairedudragon.fr" in url:
         status = soup.find("span", class_="label label-danger")
-        return status is None or "n'est pas en stock" not in status.get_text(strip=True)
+        return status is not None and "n'est pas en stock" in status.get_text(strip=True)
 
     return False
