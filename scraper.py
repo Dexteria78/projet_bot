505af16db31161812040fb1a179941bbc611c847
nicolas <nicolas@DebianBot.lan>
2025-02-16 01:54:23 +0100

    Correction stricte pour éviter les faux positifs sur Cultura et Fnac

--- scraper.py
+++ scraper.py
@@ -9,14 +9,14 @@ def check_availability(article):
 
     if "cultura.com" in url:
         status = soup.find("p", class_="stock color-red")
-        return status is None or "indisponible en ligne" not in status.get_text(strip=True).lower()
+        return status is None or (status and "indisponible en ligne" not in status.get_text(strip=True).lower())
 
     if "fnac.com" in url:
         status = soup.find("p", {"data-automation-id": "product-availability"})
-        return status is None or "stock en ligne épuisé" not in status.get_text(strip=True).lower()
+        return status is None or (status and "stock en ligne épuisé" not in status.get_text(strip=True).lower())
 
     if "lerepairedudragon.fr" in url:
         status = soup.find("span", class_="label label-danger")
-        return status is None or "n'est pas en stock" not in status.get_text(strip=True).lower()
+        return status is None or (status and "n'est pas en stock" not in status.get_text(strip=True).lower())
 
     return False
