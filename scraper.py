49315cf666f11bd1e1f1b4da3362c6a28f2aa79c
nicolas <nicolas@DebianBot.lan>
2025-03-01 21:20:32 +0100

    test23

--- scraper.py
+++ scraper.py
@@ -3,8 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 from config import DISCORD_WEBHOOK_URL, CHECK_INTERVAL, ARTICLES
 
-previous_status = {}
-
 def send_discord_message(message):
     payload = {"content": message}
     requests.post(DISCORD_WEBHOOK_URL, json=payload)
@@ -24,23 +22,27 @@ def check_availability(article):
         elif "fnac.com" in url:
             price_element = soup.select_one("span.f-priceBox_price.userPrice.checked")
             if price_element:
-                price = price_element.get_text(strip=True).replace("€", "").replace(",", ".")
+                price_text = price_element.get_text(strip=True).replace("€", "").replace(",", ".")
                 try:
-                    price = float(price)
+                    price = float(price_text)
                     if price == 6.99:
                         return True
                 except ValueError:
                     return False
             return False
         elif "lerepairedudragon.fr" in url:
-            status = soup.select_one("span.label.label-danger")
-            text = status.get_text(strip=True).lower() if status else "indisponible"
+            price_element = soup.select_one("span.f-priceBox_price.userPrice.checked")
+            if price_element:
+                price_text = price_element.get_text(strip=True).replace("€", "").replace(",", ".")
+                try:
+                    price = float(price_text)
+                    if price == 140.00:
+                        return True
+                except ValueError:
+                    return False
+            return False
         else:
             return False
-        
-        previous = previous_status.get(url, "")
-        previous_status[url] = text
-        return previous != text and "indisponible" not in text
     except requests.RequestException as e:
         print(f"Erreur sur {url}: {e}")
         return False
