4a1ea702c2245f76641dbfa407e18c251cf3f272
nicolas <nicolas@DebianBot.lan>
2025-03-01 21:59:32 +0100

    Ajout de python3-distutils pour Railway

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Met à jour les paquets et installe Chromium et ChromeDriver
-apt-get update && apt-get install -y chromium-browser chromium-driver
+# Installer les dépendances système
+apt-get update && apt-get install -y python3-distutils chromium-browser chromium-driver
 
 # Définir les variables d'environnement pour Selenium
 export PATH="/usr/lib/chromium-browser/:$PATH"
