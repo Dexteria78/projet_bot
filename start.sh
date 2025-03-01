851cae775c552a96782bb746c8ad619e29b63316
nicolas <nicolas@DebianBot.lan>
2025-03-01 21:56:20 +0100

    Correction Selenium pour Railway

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Installer Chrome et ChromeDriver sur Railway
+# Met à jour les paquets et installe Chromium et ChromeDriver
 apt-get update && apt-get install -y chromium-browser chromium-driver
+
+# Définir les variables d'environnement pour Selenium
+export PATH="/usr/lib/chromium-browser/:$PATH"
 
 # Lancer le bot
 python3 bot.py
