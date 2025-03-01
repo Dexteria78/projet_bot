e43ae24dc620d19bfcccb9b47f47cff499fab43d
nicolas <nicolas@DebianBot.lan>
2025-03-01 22:29:52 +0100

    Correction installation Chrome sur Railway

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Mettre à jour les paquets et installer Chrome et ChromeDriver
-apt-get update && apt-get install -y chromium-browser chromium-driver
+apt-get update && apt-get install -y wget unzip curl
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
 
 # Définir les variables d'environnement pour Selenium
-export CHROME_BIN="/usr/bin/chromium-browser"
-export PATH="/usr/lib/chromium-browser/:$PATH"
+export CHROME_BIN="/usr/bin/google-chrome"
+export PATH="$PATH:/usr/bin/"
+
+# Installer ChromeDriver automatiquement
+python3 -m pip install --upgrade chromedriver-autoinstaller
 
 # Lancer le bot
 python3 bot.py
